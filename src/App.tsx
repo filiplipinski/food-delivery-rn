1b7b2753163f6e47f6a784b49754712ad8b96387
Filip Lipiński <filip.lipinski@tsh.io>
2021-11-14 16:39:49 +0100

    add screens structure

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {Home} from './screens/Home';
+import {OrderDelivery} from './screens/OrderDelivery';
+import {Restaurant} from './screens/Restaurant';
+
+const Stack = createStackNavigator();
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ src/screens/OrderDelivery.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const OrderDelivery = () => {
+  return (
+    <View>
+      <Text>OrderDelivery</Text>
+    </View>
+  );
+};
